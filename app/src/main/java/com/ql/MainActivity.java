884151267e47d1a5eff1165a161aee58e52c0342
ql <ql74694664246@yahoo.com>
2018-07-06 13:54:46 +0800

    Initial Commit

---
+++ app/src/main/java/com/ql/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ql;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ql.designpatterns.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
